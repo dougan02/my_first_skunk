2a1d1c28b0ecb556a5c17dfdb525a760f306a35e
dietz <dietz057@gmail.com>
2019-03-13 19:11:22 -0500

    Added comments to custom class files and made small changes to a few variables.

--- SkunkProject/src/Player.java
+++ SkunkProject/src/Player.java
@@ -7,21 +7,28 @@ public class Player {
 	private Dice dice;
 	
 	public Player() {
-		this.playerName="Player";
-		this.playerScore=0;
-		this.dice= new Dice();
+		// Setting a default player name, player score, and dice object for default constructor.
+		this.playerName = "Player";
+		this.playerScore = 0;
+		this.dice = new Dice();
 	}
 	
 	public Player(String name) {
-		this.playerName=name;
-		this.playerScore=0;
-		this.dice= new Dice();
+		// This constructor allows you to pass a player name (otherwise the same).
+		this.playerName = name;
+		this.playerScore = 0;
+		this.dice = new Dice();
 	}
 
 	public void takeTurn() {
-		int turnScore= 0;
+		// turnScore variable represents the temporary score that a player has during his/her turn (that can be lost when rolling a skunk).
+		int turnScore = 0;
+		// choice variable used to capture keyboard input for player choice of play or hold.
 		String choice;
+		// Scanner required to take user input.
 		Scanner sc = new Scanner(System.in);
+		// continueTurn variable used in loop to track whether player turn continues or is over.
+		boolean continueTurn = true;
 		
 		do {
 			StdOut.println(this.getPlayerName() + ", would you like (p)lay or (h)old?");
@@ -29,26 +36,27 @@ public class Player {
 			
 			if (choice.equals("p")) {
 				this.dice.roll();
-				if (this.dice.getDie1LastRoll()== 1 || this.dice.getDie2LastRoll()== 1) {
+				if (this.dice.getDie1LastRoll() == 1 || this.dice.getDie2LastRoll() == 1) {
 					StdOut.println("SKUNK :D");
-					turnScore=0;
-					break;
+					turnScore = 0;
+					continueTurn = false;
 				}
 				else {
-				turnScore+= dice.getLastRoll();
-				StdOut.println(this.getPlayerName() + " turn score is " + turnScore);
+					turnScore += dice.getLastRoll();
+					StdOut.println(this.getPlayerName() + " turn score is " + turnScore);
 				}
 			}
 			else if (choice.equals("h")) {
-				break;
+				continueTurn = false;
 			}
+			// This else is included to cover if the user enters anything other than (p) or (h).  They will start the loop again.
 			else {
 				continue;
 			}
-		} while (true);
+		} while (continueTurn);
 		
-		this.playerScore+= turnScore;
-		
+		// Increment player total score with the amount of points earned during the turn (turnScore).
+		this.playerScore += turnScore;
 		StdOut.println(this.getPlayerName() + "'s turn is over!!!");
 	}
 	
